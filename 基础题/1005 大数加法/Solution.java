6ab91b3fd90f2d1ace513baeddab1e3dad43026e
徐玄清 <524058708@qq.com>
2016-04-21 09:57:22 +0800

    20160421

---
+++ 基础题/1005 大数加法/Solution.java
@@ -0,0 +1,15 @@
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Solution {
+	private static Scanner in;
+	public static void main(String[] args) {
+		in = new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		BigDecimal a = in.nextBigDecimal();
+		BigDecimal b = in.nextBigDecimal();
+		out.println(a.add(b));
+		out.flush();
+	}
+}

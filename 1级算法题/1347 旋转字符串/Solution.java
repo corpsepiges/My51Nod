d9ba31104ce5497439988f10c45c4ddba58d9d7e
徐玄清 <524058708@qq.com>
2016-04-22 17:37:05 +0800

    20160422

---
+++ 1级算法题/1347 旋转字符串/Solution.java
@@ -0,0 +1,13 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Solution {
+	private static Scanner in;
+	public static void main(String[] args) {
+		in = new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		String str=in.next();
+		out.write(str.length()%2==0&&str.substring(0, str.length()/2).equals(str.substring(str.length()/2, str.length()))?"YES\n":"NO\n");
+		out.flush();
+	}
+}
